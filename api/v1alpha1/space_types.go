aca7aae1822817d08700ac4845527699546c1d59
Ismail KABOUBI <ikaboubi@gmail.com>
2023-01-13 15:03:04 +0100

    init the kubebuilder scaffold and describe the first api spec

---
+++ api/v1alpha1/space_types.go
@@ -0,0 +1,62 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// SpaceSpec defines the desired state of Space
+type SpaceSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+}
+
+// SpaceStatus defines the observed state of Space
+type SpaceStatus struct {
+	// NamespaceName the name of the created underlying namespace.
+	NamespaceName string `json:"namespaceName,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Space is the Schema for the spaces API
+type Space struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   SpaceSpec   `json:"spec,omitempty"`
+	Status SpaceStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// SpaceList contains a list of Space
+type SpaceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Space `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Space{}, &SpaceList{})
+}
