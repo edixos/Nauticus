3d0ab4a1aea0e4cef2dce66fc502df9b531ee20f
Ismail KABOUBI <ikaboubi@gmail.com>
2023-01-18 17:36:41 +0100

    run the controller within the tests

---
+++ controllers/suite_test.go
@@ -0,0 +1,105 @@
+/*
+Copyright 2023.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+    "path/filepath"
+    "testing"
+    "time"
+
+    . "github.com/onsi/ginkgo/v2"
+    . "github.com/onsi/gomega"
+    "github.com/onsi/gomega/gexec"
+    ctrl "sigs.k8s.io/controller-runtime"
+
+    "k8s.io/client-go/kubernetes/scheme"
+    "k8s.io/client-go/rest"
+    "sigs.k8s.io/controller-runtime/pkg/client"
+    "sigs.k8s.io/controller-runtime/pkg/envtest"
+    logf "sigs.k8s.io/controller-runtime/pkg/log"
+    "sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+    nauticusiov1alpha1 "github.com/edixos/nauticus/api/v1alpha1"
+    //+kubebuilder:scaffold:imports
+)
+
+// These tests use Ginkgo (BDD-style Go testing framework). Refer to
+// http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
+
+var cfg *rest.Config
+var k8sClient client.Client
+var testEnv *envtest.Environment
+
+func TestAPIs(t *testing.T) {
+    RegisterFailHandler(Fail)
+
+    RunSpecs(t, "Controller Suite")
+}
+
+var _ = BeforeSuite(func() {
+    logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
+
+    By("bootstrapping test environment")
+    testEnv = &envtest.Environment{
+        CRDDirectoryPaths:     []string{filepath.Join("..", "config", "crd", "bases")},
+        ErrorIfCRDPathMissing: true,
+    }
+
+    var err error
+    // cfg is defined in this file globally.
+    cfg, err = testEnv.Start()
+    Expect(err).NotTo(HaveOccurred())
+    Expect(cfg).NotTo(BeNil())
+
+    err = nauticusiov1alpha1.AddToScheme(scheme.Scheme)
+    Expect(err).NotTo(HaveOccurred())
+
+    //+kubebuilder:scaffold:scheme
+
+    k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+    Expect(err).NotTo(HaveOccurred())
+    Expect(k8sClient).NotTo(BeNil())
+
+    k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
+        Scheme: scheme.Scheme,
+    })
+    Expect(err).ToNot(HaveOccurred())
+    err = (&SpaceReconciler{
+        Client: k8sManager.GetClient(),
+        Scheme: k8sManager.GetScheme(),
+        Log:    ctrl.Log.WithName("controllers").WithName("Space"),
+    }).SetupWithManager(k8sManager)
+    Expect(err).ToNot(HaveOccurred())
+
+    go func() {
+        defer GinkgoRecover()
+        err = k8sManager.Start(ctrl.SetupSignalHandler())
+        Expect(err).ToNot(HaveOccurred(), "failed to run manager")
+        gexec.KillAndWait(4 * time.Second)
+
+        // Teardown the test environment once controller is fnished.
+        // Otherwise from Kubernetes 1.21+, teardon timeouts waiting on
+        // kube-apiserver to return
+        err := testEnv.Stop()
+        Expect(err).ToNot(HaveOccurred())
+    }()
+
+    k8sClient = k8sManager.GetClient()
+    Expect(k8sClient).ToNot(BeNil())
+
+})
+
+var _ = AfterSuite(func() {
+    By("tearing down the test environment")
+})
--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/edixos/nauticus
 go 1.19
 
 require (
+	github.com/go-logr/logr v1.2.3
 	github.com/onsi/ginkgo/v2 v2.1.4
 	github.com/onsi/gomega v1.19.0
+	k8s.io/api v0.25.0
 	k8s.io/apimachinery v0.25.0
 	k8s.io/client-go v0.25.0
 	sigs.k8s.io/controller-runtime v0.13.1
@@ -26,7 +28,6 @@ require (
 	github.com/emicklei/go-restful/v3 v3.8.0 // indirect
 	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
-	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/zapr v1.2.3 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.19.5 // indirect
@@ -69,7 +70,6 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/api v0.25.0 // indirect
 	k8s.io/apiextensions-apiserver v0.25.0 // indirect
 	k8s.io/component-base v0.25.0 // indirect
 	k8s.io/klog/v2 v2.70.1 // indirect
